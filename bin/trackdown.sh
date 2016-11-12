5741de155f40e1af776eb891c9dbc0ebc6897e65
Martin Goellnitz <martin@goellnitz.de>
2016-11-12 23:53:39 +0100

    Allow for spaces in sprint/iteration/milestone/release names

--- bin/trackdown.sh
+++ bin/trackdown.sh
@@ -78,7 +78,8 @@ if [ "$CMD" = "roadmap" ] ; then
   fi
   echo "# Roadmap"
   echo ""
-  for r in `grep "^\*[A-Za-z0-9\._]*\*" $ISSUES|cut -d '*' -f 2|sort|uniq` ; do
+  for rr in `grep "^\*[A-Za-z0-9][A-Za-z0-9\._\ ]*\*" $ISSUES|cut -d '*' -f 2|sort|uniq|sed -e 's/\ /__/g'` ; do
+    r=`echo $rr|sed -e 's/__/ /g'`
     TOTAL=`grep -B2 "^\*$r\*" $ISSUES|grep "^\#\#\ "|sed -e 's/^\#\#\ /\#\#\# /g'|wc -l`
     RESOLVED=`grep -B2 "^\*$r\*" $ISSUES|grep "^\#\#\ "|sed -e 's/^\#\#\ /\#\#\# /g'|grep -i '(resolved)'|wc -l`
     PROGRESS=`grep -B2 "^\*$r\*" $ISSUES|grep "^\#\#\ "|sed -e 's/^\#\#\ /\#\#\# /g'|grep -i '(in progress)'|wc -l`
