ce8109fe12f2356a424dd12c56db6a1b733439a6
Martin Goellnitz <martin@goellnitz.de>
2019-02-04 00:33:08 +0100

    Mark test for debugging

--- bin/trackdown-hook.sh
+++ bin/trackdown-hook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2015-2018 Martin Goellnitz
+# Copyright 2015-2019 Martin Goellnitz
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,8 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-
+echo "BEFORE $0"
 DIR=`dirname $0`
+echo "AFTER"
 . $DIR/trackdown-lib.sh
 CWD=`pwd`
 windUp trackdown
